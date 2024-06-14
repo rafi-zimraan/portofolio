daa1f32f1e3402a384cb8c5d1658a0debb74044d
Syenvi <ryujinkun34@gmail.com>
2024-06-14 10:05:28 +0700

    'test'

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/react-portofolioRafi-frontend/",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "@assets": resolve(__dirname, "src/assets"),
+    },
+  },
 });

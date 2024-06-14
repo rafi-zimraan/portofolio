baea690927e0990247a571559d3877a3463a68f3
Syenvi <ryujinkun34@gmail.com>
2024-06-14 09:55:29 +0700

    'fix-typo'

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../../App.css";
-import Toogle from "../toogle";
+import Toogle from "../Toogle";
 
 const Header = () => {
   window.addEventListener("scroll", function () {
